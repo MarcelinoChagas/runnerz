a928950d1c07747c3f716b08baa35d99be59cd25
Marcelino Chagas <22219284+MarcelinoChagas@users.noreply.github.com>
2024-11-17 20:31:08 -0300

    Remember about springboot

--- src/main/java/dev/marcelinochagas/runnerz/Application.java
+++ src/main/java/dev/marcelinochagas/runnerz/Application.java
@@ -2,12 +2,14 @@ package dev.marcelinochagas.runnerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.print.StreamPrintService;
 
 @SpringBootApplication
-public class RunnerzApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunnerzApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
